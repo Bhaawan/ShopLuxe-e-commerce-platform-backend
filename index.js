654bfa8f10a2091ad5b32c7bf71ad10a70c37267
Bhaawan_Shrivastav <bhaawan272002@gmail.com>
2024-09-07 17:27:35 +0530

    Backend part-2: User, Cart and Orders

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
 const express=require('express');
 const server=express();
 const mongoose=require('mongoose');
+const cors=require('cors')
+
 const { createProduct } = require('./controller/Product');
 const productsRouter=require('./routes/Products.js')
 const categoriesRouter=require('./routes/Categories.js')
 const brandsRouter=require('./routes/Brands.js')
-const cors=require('cors')
-
+const userRouter=require('./routes/Users.js')
+const authRouter=require('./routes/Auth.js')
+const cartRouter=require('./routes/Carts.js')
+const orderRouter=require('./routes/Orders.js')
 
 server.use(cors({
     exposedHeaders:['X-Total-Count']
 }))
 server.use(express.json());
-server.use('/products', productsRouter.router)
-server.use('/categories', categoriesRouter.router)
-server.use('/brands', brandsRouter.router)
+server.use('/products', productsRouter.router);
+server.use('/categories', categoriesRouter.router);
+server.use('/brands', brandsRouter.router);
+server.use('/users',userRouter.router);
+server.use('/auth',authRouter.router);
+server.use('/cart',cartRouter.router);
+server.use('/orders',orderRouter.router);
+
 
 main().catch(err=>console.log(err));
 
