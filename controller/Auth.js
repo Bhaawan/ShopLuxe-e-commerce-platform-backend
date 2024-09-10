fdcff65fbefb4fb0cc1350a9ab6230d2b04159b6
Bhaawan_Shrivastav <bhaawan272002@gmail.com>
2024-09-10 17:12:57 +0530

    Backend Part-5: passport implemented with local/JWT

--- controller/Auth.js
+++ controller/Auth.js
@@ -1,34 +1,39 @@
 const { User } = require("../model/User");
+const crypto=require('crypto');
+const { sanitizeUser } = require("../services/common");
+const SECRET_KEY='SECRET_KEY';
+const jwt = require('jsonwebtoken');
+
 
 
 exports.createUser=async(req,res)=>{
-    const user=new User(req.body);
     try{ 
-        const result=await user.save();
-        res.status(200).json({id:result.id, role:result.role});
+        const salt = crypto.randomBytes(16);
+        crypto.pbkdf2(req.body.password, salt, 310000, 32, 'sha256', 
+        async function(err, hashedPassword) {
+            const user=new User({...req.body, password:hashedPassword, salt});
+            const result=await user.save();
+
+            req.login(sanitizeUser(result),(err)=>{
+                if(err){
+                    res.status(400).json(err);
+                }
+                else
+                {
+                    const token = jwt.sign(sanitizeUser(result),SECRET_KEY);
+                    res.status(200).json(token);
+                }
+            })
+        })
     }catch(err){
         res.status(400).json(err);
     }
 };
 
 exports.loginUser=async(req,res)=>{ 
-    try{ 
-        const user= await User.findOne({email:req.body.email});
+    res.json(req.user)
+};
 
-        if(!user)
-        {
-            res.status(401).json({message:'no such user email'});
-        }
-        else if(user.password===req.body.password)
-        {
-            res.status(200).json({id:user.id, role:user.role});
-        }
-        else
-        {
-            res.status(401).json({message:'invalid credentials'});
-        }
-
-    }catch(err){
-        res.status(400).json(err);
-    }
+exports.checkUser=async(req,res)=>{ 
+    res.json({status:'success', user:req.user})
 };
--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express=require('express');
 const server=express();
 const mongoose=require('mongoose');
-const cors=require('cors')
+const cors=require('cors');
+const session = require('express-session');
+const passport = require('passport');
+const crypto = require('crypto');
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const jwt = require('jsonwebtoken');
 
 const { createProduct } = require('./controller/Product');
 const productsRouter=require('./routes/Products.js')
@@ -10,19 +16,97 @@ const brandsRouter=require('./routes/Brands.js')
 const userRouter=require('./routes/Users.js')
 const authRouter=require('./routes/Auth.js')
 const cartRouter=require('./routes/Carts.js')
-const orderRouter=require('./routes/Orders.js')
+const orderRouter=require('./routes/Orders.js');
+const { User } = require('./model/User.js');
+const { isAuth, sanitizeUser } = require('./services/common.js');
+
+const SECRET_KEY='SECRET_KEY';
+
+var opts = {}
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = SECRET_KEY;
+
+
+const LocalStrategy=require('passport-local').Strategy;
+
+server.use(session({
+    secret: 'keyboard cat',
+    resave: false, // don't save session if unmodified
+    saveUninitialized: false, // don't create session until something stored
+}));
+server.use(passport.authenticate('session'));
 
 server.use(cors({
     exposedHeaders:['X-Total-Count']
 }))
 server.use(express.json());
-server.use('/products', productsRouter.router);
-server.use('/categories', categoriesRouter.router);
-server.use('/brands', brandsRouter.router);
-server.use('/users',userRouter.router);
+server.use('/products', isAuth(), productsRouter.router);
+server.use('/categories',isAuth(), categoriesRouter.router);
+server.use('/brands', isAuth(), brandsRouter.router);
+server.use('/users', isAuth(),userRouter.router);
 server.use('/auth',authRouter.router);
-server.use('/cart',cartRouter.router);
-server.use('/orders',orderRouter.router);
+server.use('/cart', isAuth(),cartRouter.router);
+server.use('/orders', isAuth(),orderRouter.router);
+
+passport.use('local',new LocalStrategy(
+    {username:'email'},
+    async function(email, password, done) {
+        try{ 
+            const user= await User.findOne({email:email});
+            if(!user)
+            {
+                return done(null,false,{message:'no such user email'});
+            }
+
+            crypto.pbkdf2(
+                password,
+                user.salt,
+                310000,
+                32,
+                'sha256',
+                async function (err, hashedPassword) {
+                  if (!crypto.timingSafeEqual(user.password, hashedPassword)) {
+                    return done(null, false, { message: 'invalid credentials' });
+                  }
+                  const token = jwt.sign(sanitizeUser(user),SECRET_KEY);
+                  done(null, token); // this lines sends to serializer
+                }
+            );
+        }catch(err){
+            done(err)
+        }
+    }
+));
+
+passport.use('jwt', new JwtStrategy(opts, async function(jwt_payload, done) {
+    console.log({jwt_payload});
+    try{
+        const user=await User.findOne({id: jwt_payload.sub})
+        if (user) {
+            return done(null, sanitizeUser(user));
+        } else {
+            return done(null, false);
+            // or you could create a new account
+        }
+    }
+    catch(err){
+        return done(err, false);
+    }
+}));
+
+passport.serializeUser(function(user, cb) {
+    console.log('serialize',user)
+    process.nextTick(function() {
+      return cb(null,{id:user.id, role:user.role});
+    });
+});
+  
+  passport.deserializeUser(function(user, cb) {
+    console.log('de-serialize',user)
+    process.nextTick(function() {
+      return cb(null, user);
+    });
+});
 
 
 main().catch(err=>console.log(err));
@@ -31,11 +115,6 @@ async function main(){
     await mongoose.connect('mongodb://127.0.0.1:27017/ecommerce');
     console.log("database connected");   
 }
-
-
-server.get('/',(req,res)=>{
-    res.json({status:"Success"})
-})
 
 
 server.listen(8080, ()=>{
